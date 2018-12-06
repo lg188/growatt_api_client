6d17f329b9c66f04f771c49ca82628118d76e026
lg188 <lander.ghekiere@gmail.com>
2018-12-06 10:22:35 +0100

    Code linted

--- growatt.py
+++ growatt.py
@@ -1,104 +1,136 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""
+A client for the Growatt API
+"""
 
 from enum import IntEnum
-from pprint import pprint
-from sys import exit
+import sys
 import datetime
 import hashlib
 import json
-import netrc
+import netrc as Netrc
+import time as Time
 import requests
 
+
 # Function to create hash for your password to login with
-def hash_password(password):
+def hash_password(password_raw):
     """
     Normal MD5, except add c if a byte of the digest is less than 10.
     """
-    password_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
+    password_md5 = hashlib.md5(password_raw.encode('utf-8')).hexdigest()
     for i in range(0, len(password_md5), 2):
         if password_md5[i] == '0':
             password_md5 = password_md5[0:i] + 'c' + password_md5[i + 1:]
     return password_md5
 
+
 class Timespan(IntEnum):
+    """
+    The timespans supported by the Growatt API.
+    """
+    now = 0
     day = 1
     month = 2
     year = 3
 
 
 class GrowattApi:
+    """
+    A collection of all known API calls for the API.
+    """
+
     server_url = 'https://server.growatt.com/'
 
     def __init__(self):
         self.session = requests.Session()
 
     def get_url(self, page):
+        """
+        A helper function to parse URLs.
+        """
         return self.server_url + page
 
-    def login(self, username, password):
-        password_md5 = hash_password(password)
+    def login(self, user, raw_password):
+        """
+        Log in on the API.
+        """
+        password_md5 = hash_password(raw_password)
         response = self.session.post(self.get_url('LoginAPI.do'), data={
-            'userName': username,
+            'userName': user,
             'password': password_md5
         })
         data = json.loads(response.content.decode('utf-8'))
         return data['back']
 
     def plant_list(self, user_id):
+        """
+        Retrieve the list of all plants.
+        """
         response = self.session.get(self.get_url('PlantListAPI.do'),
                                     params={'userId': user_id},
                                     allow_redirects=False)
         if response.status_code != 200:
-            raise RuntimeError("Request failed: %s", response)
+            raise RuntimeError(f"Request failed: {response}")
         data = json.loads(response.content.decode('utf-8'))
         return data['back']
 
-    def plant_detail(self, plant_id, timespan, date):
+    def plant_detail(self, plant_id, timespan, time):
+        """
+        Retrieves the metric data of a plant.
+        Based on the timespan
+        """
         assert timespan in Timespan
-        if timespan == Timespan.day:
-            date_str = date.strftime('%Y-%m-%d')
+        if timespan == Timespan.now:
+            time_str = Time.time()
+        elif timespan == Timespan.day:
+            time_str = time.strftime('%Y-%m-%d')
         elif timespan == Timespan.month:
-            date_str = date.strftime('%Y-%m')
+            time_str = time.strftime('%Y-%m')
         elif timespan == Timespan.year:
-            date_str = date.strftime('%Y')
+            time_str = time.strftime('%Y')
 
         response = self.session.get(self.get_url('PlantDetailAPI.do'), params={
             'plantId': plant_id,
             'type': timespan.value,
-            'date': date_str
+            'date': time_str
         })
         data = json.loads(response.content.decode('utf-8'))
-        pprint(data)
         return data['back']
+
 
 # Set username and password here
 if __name__ == "__main__":
+    sys.tracebacklimit = 0
     try:
-        MyNetrc = netrc.netrc()
-        credentials = MyNetrc.authenticators("growat.com")
-        if credentials:
-            username = credentials[0] 
-            password = credentials[2] 
+        NETRC = Netrc.netrc()
+        CREDENTIALS = NETRC.authenticators("growat.com")
+        if CREDENTIALS:
+            USERNAME = CREDENTIALS[0]
+            PASSWORD = CREDENTIALS[2]
         else:
-            print("No netrc entry")
-            exit(-1)
-    except netrc.NetrcParseError:
-        print("Unable to parse netrc")
-        exit(-1)
- 
-    api = GrowattApi()
-    login_res = api.login(username, password)
-    if login_res['success'] == False:
-        print("Login failed.")
-        exit(-1)
-    user_id = login_res['userId']
+            raise RuntimeError("Netrc entry missing")
+    except NETRC.NetrcParseError as err:
+        raise RuntimeError(f"Could not parse the netrc file: {err}")
+
+    API = GrowattApi()
+
+    LOGIN = API.login(USERNAME, PASSWORD)
+    if not LOGIN['success']:
+        raise RuntimeError("Login failed")
+
+    USER_ID = LOGIN['userId']
+
     # Get basic plant info
-    plant_info = api.plant_list(user_id)
+    PLANT_INFO = API.plant_list(USER_ID)
 
     # Get detailed plant info
-    plant_id = plant_info['data'][0]['plantId']
-    plant_detail = api.plant_detail(plant_id, Timespan.day, datetime.datetime.now())
-    currentEnergy = plant_detail['plantData']['currentEnergy']
-    print(currentEnergy)
-    
+    PLANT_ID = PLANT_INFO['data'][0]['plantId']
+    PLANT_DETAIL = API.plant_detail(
+        PLANT_ID, Timespan.day, datetime.datetime.now())
+    if not PLANT_DETAIL['success']:
+        if PLANT_DETAIL['errCode'] == '201':
+            raise RuntimeError("The date string is invalied.")
+        raise RuntimeError("The request returned invalid data.")
+    CURRENT_ENERGY = PLANT_DETAIL['plantData']['currentEnergy']
+    print(CURRENT_ENERGY)
